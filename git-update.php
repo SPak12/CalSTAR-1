c9a8d318a8189d3c301348751342d763c6183d82
Sean Pak <seantwelve3@gmail.com>
2017-10-06 18:42:51 -0700

    Update GitHub Files

--- git-update.php
+++ git-update.php
@@ -1,10 +1,38 @@
 <?php
 
-file_put_contents('git-update-output.json', json_encode($_POST));
-/*if (array_key_exists('X-GitHub-Event', $_POST)) {
-	echo $_POST['X-GitHub-Event'];
-	//exec('../update.sh');
-	exec('touch we-got-an-update');
-}*/
+/*
+ * This key is a SHA hash of the "secret key" set up in the
+ * Github webhook. This must be edited after changing the secret!
+ * To get the string to put here, do any push (so that Github sends
+ * a webhook request) and then open up the details in the settings
+ * page to see the payload: copy the X-Hub-Signature value here.
+ */
+$SHA_SECRET_KEY = "sha1=bf45f600746414aba51131d64e170e8b2f504b95";
+
+function getRequestHeaders() {
+	$headers = array();
+	foreach($_SERVER as $key => $value) {
+		if (substr($key, 0, 5) <> 'HTTP_') {
+			continue;
+		}
+		$header = str_replace(' ', '-', ucwords(str_replace('_', ' ', strtolower(substr($key, 5)))));
+		$headers[$header] = $value;
+	}
+	return $headers;
+}
+$headers = getRequestHeaders();
+
+if (array_key_exists('X-Hub-Signature', $headers)) {
+	if ($headers['X-Hub-Signature'] === $SHA_SECRET_KEY) {
+		exec("touch ../trying-update");
+		exec('../update.sh > update-output 2>&1');
+	} else {
+		exec("touch ../bad-signature");
+		echo "go away hacker >(";
+	}
+} else {
+	exec("touch ../no-key");
+	echo "NOOOPE";
+}
 
 ?>
